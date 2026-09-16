9efc327d25bbbe3742ea0bece120367965e52efa
rodr1go258 <rodrigozeballos521@gmail.com>
2026-09-16 19:54:11 -0400

    feat: agregar ruta de saludo

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const PORT = 3000;
 app.get('/', (req, res) => {
   res.send('App Oficial - Grupo 5 funcionando correctamente 🚀');
 });
-
+app.get('/saludo', (req, res) => {
+  res.send('Hola, esta es la ruta de saludo del Grupo 5');
+});
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
