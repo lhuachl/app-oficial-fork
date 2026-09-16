a1fcfc438f6b1335e008085822dae6e7a9aa42da
rodr1go258 <rodrigozeballos521@gmail.com>
2026-09-16 19:58:40 -0400

    feat: agregar ruta de estado del servidor

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('App Oficial - Grupo 5 funcionando correctamente 🚀');
 });
 
+app.get('/estado', (req, res) => {
+  res.send('Servidor activo y funcionando correctamente');
+});
+
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
